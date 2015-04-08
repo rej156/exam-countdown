2cd9794b294e15c866af155e712c207198cc7d2f
Eric Juta <ericjohnjuta@gmail.com>
2015-04-08 15:56:03 +0100

    Learned how to add persist to localstorage middleware and initialise-db merged with localstorage

--- src/cljs/exam_countdown/handlers.cljs
+++ src/cljs/exam_countdown/handlers.cljs
@@ -1,13 +1,24 @@
 (ns exam-countdown.handlers
   (:require [exam-countdown.db :refer [default-value valid-schema?]]
+            [cljs.reader]
             [re-frame.core :refer [register-handler debug after path trim-v]]))
 
 
 ;; -- Middleware --------------------------------------------------------------
 ;; See:  https://github.com/Day8/re-frame/wiki/Debugging-Event-Handlers
 ;;
+(defn selected->ls! [exams]
+  (.setItem js/localStorage "exams" (str exams)))
+
+(defn ls->exams []
+  (some->> (.getItem js/localStorage "exams")
+           (cljs.reader/read-string)))
+
+(def ->ls (after selected->ls!))
+
 (def standard-middlewares
-  [trim-v
+  [->ls
+   trim-v
    ;; (if goog.DEBUG log-ex)    ;; will be available in v0.3.0
    ])
 
@@ -17,17 +28,17 @@
 ;; -- Handlers ----------------------------------------------------------------
 
 (register-handler                 ;; disptached to on app startup
-  :initialise-db                  ;; event id being handled
-  (fn  [_ _]                      ;; the handler
-    default-value))               ;; all hail the new state
+ :initialise-db                  ;; event id being handled
+ (fn  [_ _]                      ;; the handler
+   (merge default-value (ls->exams))))               ;; all hail the new state
 
 
 (register-handler                 ;; handlers changes the footer filter
-  :set-name                       ;; event-id
-  [standard-middlewares (path :name)]
-  (fn                             ;; handler
-    [db [new-name]]               ;; because of trim-v, not [_ new-name]
-    new-name))
+ :set-name                       ;; event-id
+ [standard-middlewares (path :name)]
+ (fn                             ;; handler
+   [db [new-name]]               ;; because of trim-v, not [_ new-name]
+   new-name))
 
 (register-handler
  :select-exam
